fa84c009b8f9c60034793504240c6e087ec11850
rauland <30706122+rauland@users.noreply.github.com>
2022-11-14 08:15:33 +1100

    changed ips

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ if not ini.is_file():
     config = configparser.ConfigParser()
     config['DEFAULT']={'windowsposition':'200x60-0-50'}
     config['scale']= {}
-    config['scale']=  { 'ip':'192.168.10.200,192.168.10.201,192.168.10.202', # comma for mutiple ips
+    config['scale']=  { 'ip':'192.168.1.100,192.168.1.101,192.168.1.102', # comma for mutiple ips
                         'port':'1749'}
                    
     with open('config.ini', 'w') as configfile:
