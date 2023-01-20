3b2cf7f56e260aa90c684bc4d52a9f992286b6a8
rauland <30706122+rauland@users.noreply.github.com>
2023-01-20 14:34:55 +1100

    added msi packaging to build script

--- build.ps1
+++ build.ps1
@@ -1,1 +1,14 @@
-pyinstaller --onefile --noconsole --icon=icon.ico main.pyw
+pyinstaller --onefile --noconsole --icon=icon.ico main.pyw 
+
+# PSMSI
+New-Installer -ProductName "WeighBridge Scale" -UpgradeCode '1f72da53-3058-4b30-98de-99b4e0cf1ea0' -Content {
+    New-InstallerDirectory -PredefinedDirectory "ProgramFilesFolder"  -Content {
+       New-InstallerDirectory -DirectoryName "WeighBridge Scale" -Content {
+          New-InstallerFile -Source .\dist\config.ini
+          New-InstallerFile -Source .\dist\main.exe -id main
+       }
+    }
+    New-InstallerDirectory -PredefinedDirectoryName DesktopFolder -Content {
+        New-InstallerShortcut -Name 'WeightBridge Scale' -FileId 'main' -WorkingDirectoryId 'MyDir'
+    }
+ } -OutputDirectory (Join-Path $PSScriptRoot "output") -RequiresElevation
