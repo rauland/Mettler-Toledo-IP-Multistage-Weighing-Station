b45ef1ea9315afd7fa2655fdd2b587d51f74d778
rauland <30706122+rauland@users.noreply.github.com>
2024-03-03 00:02:59 +1100

    added scale server

---
+++ server.py
@@ -0,0 +1,39 @@
+import socketserver
+import time
+import random
+
+# Mimic Scale Data
+def scale(weight = 20, plus = 0):
+    number = weight + plus
+    formatted_number = "{:04d}".format(number)
+    return number, f"zero {formatted_number} one two three"
+
+class TCPHandler(socketserver.BaseRequestHandler):
+    """
+    The request handler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+
+    def handle(self):
+        print("{} Connected.".format(self.client_address[0]))
+        weight, text = scale()
+        while True:
+            self.request.sendall(text.encode())
+            time.sleep(0.01)
+
+            # This will increase scale weight slowly
+            if random.randint(0,1000) > 750:
+               weight, text = scale(weight, 20)
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 1749
+
+    # Create the server, binding to localhost on port 1749
+    server = socketserver.TCPServer((HOST, PORT), TCPHandler)
+
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
